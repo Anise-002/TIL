72c2e0ec6012580f421060c3a38d45bba26067bb
Anise-002 <n-u-002@naver.com>
2022-05-31 17:18:15 +0900

    TIL 클론 인터랙티브 - 마지막 문단 애니메이션

--- 5. 인터랙티브 웹 사이트 클론/project/js/main.js
+++ 5. 인터랙티브 웹 사이트 클론/project/js/main.js
@@ -110,7 +110,7 @@
             scrollHeight: 0,
             objs : {
                 container : document.querySelector('#scroll-section-3'),
-                cnavasCaption : document.querySelector('.canvas-caption'),
+                canvasCaption : document.querySelector('.canvas-caption'),
                 canvas : document.querySelector('.image-blend-canvas'),
                 context : document.querySelector('.image-blend-canvas').getContext('2d'),
                 imagesPath : [
@@ -125,7 +125,9 @@
                 rect2X : [0, 0, {start : 0, end : 0}],
                 rectStartY : 0,
                 blendHeight : [0, 0, {start: 0, end:0}],
-                canvas_scale : [0, 0, {start: 0, end:0}]
+                canvas_scale : [0, 0, {start: 0, end:0}],
+                canvasCaption_opacity : [0, 1, {start: 0, end : 0}],
+                canvasCaption_translateY : [35, 0, {start: 0, end : 0}],
             }
 
         }
@@ -441,6 +443,14 @@
                     if(scrollRatio > values.canvas_scale[2].end && 0 < values.canvas_scale[2].end){
                         objs.canvas.classList.remove('sticky');
                         objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
+                        
+                        values.canvasCaption_opacity[2].start = values.canvas_scale[2].end;
+                        values.canvasCaption_opacity[2].end = values.canvasCaption_opacity[2].start + 0.1;
+                        values.canvasCaption_translateY[2].start = values.canvas_scale[2].end;
+                        values.canvasCaption_translateY[2].end = values.canvasCaption_opacity[2].start + 0.1;
+                        objs.canvasCaption.style.opacity = calcValues(values.canvasCaption_opacity, currentYOffset);
+                        objs.canvasCaption.style.transform = `translate3d(0, ${calcValues(values.canvasCaption_translateY, currentYOffset)}%, 0)`;
+
                     }
 
                 }
