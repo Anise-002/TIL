615f666621567d26ba7f5fd7e9efbcc41201615f
Anise-002 <n-u-002@naver.com>
2022-06-23 23:18:25 +0900

    6.React 기본 - React.memo 기초2

--- 6. 잘라먹는 리액트/simplediary/src/OptimizeTest.js
+++ 6. 잘라먹는 리액트/simplediary/src/OptimizeTest.js
@@ -1,36 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
-const Textview = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
     useEffect(() => {
-        console.log('Update :: Text :' + text);
-    })
-    return <div>{text}</div>
-});
-
-const CountView = React.memo(({ count }) => {
-    useEffect(() => {
-        console.log('Update :: count :' + count);
+        console.log('Counter A Update - count' + count);
     })
     return <div>{count}</div>
 });
 
+
+const CounterB = ({ obj }) => {
+    useEffect(() => {
+        console.log(`Counter B Update - obj : ${obj.count}`);
+    });
+    return <div>{obj.count}</div>
+};
+
+const areEqual = (prevProps, nextProps) => {
+    if (prevProps.obj.count === nextProps.obj.count) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const MemorizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
 
     const [count, setCount] = useState(1);
-    const [text, setText] = useState(" ");
+    const [obj, setObj] = useState({
+        count: 1
+    })
 
 
     return (
         <div style={{ padding: 50 }}>
             <div>
-                <h2>count</h2>
-                <CountView count={count} />
-                <button onClick={() => setCount(count + 1)}>+</button>
+                <h2>Count A</h2>
+                <CounterA count={count} />
+                <button onClick={() => { setCount(count) }}>A button</button>
             </div>
             <div>
-                <h2>text</h2>
-                <Textview text={text} />
-                <input value={text} onChange={(e) => { setText(e.target.value) }} />
+                <h2>Count B</h2>
+                <MemorizedCounterB obj={obj} />
+                <button onClick={() => setObj({
+                    count: obj.count,
+                })}>
+                    B button
+                </button>
             </div>
         </div>
     )
